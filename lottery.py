cdda1a733cf1dc21591656b60d65851b839464e8
Rod <rodpat@gmail.com>
2023-04-17 19:01:39 -0400

    adding method calculate_powerball_number, this method return a random number between 1 and 26

--- lottery.py
+++ lottery.py
@@ -16,6 +16,9 @@ class Lottery:
                 count += 1
         return self.winner_numbers
 
+    def calculate_powerball_number(self):
+        return random.randrange(1,26)
+
     def show_numbers(self):
         self.winner_numbers.sort()
         for number in self.winner_numbers:
