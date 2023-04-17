3ea3840af9423a1ffb24792540688f26158af953
Rod <rodpat@gmail.com>
2023-04-17 18:45:13 -0400

    create class Lottery with two new methods

--- lottery.py
+++ lottery.py
@@ -1,1 +1,22 @@
-#testing
+import random
+
+
+class Lottery:
+
+    def __init__(self, numbers):
+        self.numbers = numbers
+        self.winner_numbers = []
+
+    def calculate_numbers_lottery(self):
+        count = 0
+        while count < 5:
+            number = random.randrange(1, 69)
+            if number not in self.winner_numbers:
+                self.winner_numbers.append(number)
+                count += 1
+        return self.winner_numbers
+
+    def show_numbers(self):
+        self.winner_numbers.sort()
+        for number in self.winner_numbers:
+            print(number, end=' ')
