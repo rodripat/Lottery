153e7864559e70cf2febb47d1d70c0f0152c45ec
Rod <rodpat@gmail.com>
2023-04-21 18:17:30 -0400

    adding method lottery_prizes

--- lottery.py
+++ lottery.py
@@ -19,7 +19,25 @@ class Lottery:
     def calculate_powerball_number(self):
         return random.randrange(1,26)
 
+    def lottery_prizes(self, hits, mistery_ball):
+        if hits == 5 and mistery_ball is True:
+            return "Jackpot"
+        elif hits == 5 and mistery_ball is False:
+            return "1000000"
+        elif hits == 4 and mistery_ball is True:
+            return "50000"
+        elif (hits == 4 and mistery_ball is False) or (hits == 3 and mistery_ball is True):
+            return "100"
+        elif(hits == 3 and mistery_ball is False) or (hits == 2 and mistery_ball is True):
+            return "7"
+        elif (hits == 1 and mistery_ball is True) or (hits == 0 and mistery_ball is True):
+            return "4"
+        else:
+            return "No prizes this time."
+
     def show_numbers(self):
         self.winner_numbers.sort()
         for number in self.winner_numbers:
             print(number, end=' ')
+
+
